0ed433b387e29dc53490f5fb143869edf31c1f0f
Mohammed Yazji <mw.alyazji@gmail.com>
2025-11-18 17:07:14 +0200

    implement signup endpoint logic

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,12 +1,75 @@
+import catchAsync from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+// function to generate code for verification
+const generateVerificationToken = () => {
+  const array = new Uint32Array(1);
+  crypto.getRandomValues(array);
+  return (array[0] % 1_000_000).toString().padStart(6, "0");
+};
+
+// function to generate token
+const generateToken = async (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return token;
+};
+
+// function to set cookies
+const setCookies = (res, token) => {
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
+  });
+};
+
+///////////////////
 // Main Controllers
-export const signup = async (req, res) => {
-  res.send("Signup Route");
-};
+export const signup = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-export const login = async (req, res) => {
-  res.send("Login Route");
-};
+  // check if user exist
+  const isUserExist = await User.findOne({ email });
+  if (isUserExist) {
+    return next(new AppError("User already exists", 409));
+  }
 
-export const logout = async (req, res) => {
-  res.send("Logout Route");
-};
+  // generate verification token
+  const verificationToken = generateVerificationToken();
+
+  // create a new user
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+    verificationToken,
+    verificationTokenExpiresAt: Date.now() + 5 * 60 * 60 * 1000,
+  });
+
+  // make user authenticated
+  // 1) generate tokens
+  const token = await generateToken(newUser._id);
+  // 2) set cookies
+  setCookies(res, token);
+
+  res.status(201).json({
+    status: "success",
+    message: "User created successfully",
+    user: {
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      role: newUser.role,
+    },
+  });
+});
+
+export const login = catchAsync(async (req, res, next) => {});
+
+export const logout = catchAsync(async (req, res, next) => {});
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
   {
@@ -7,7 +8,7 @@ const userSchema = mongoose.Schema(
       required: true,
     },
     email: {
-      type: email,
+      type: String,
       required: true,
       unique: true,
     },
@@ -30,6 +31,32 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// user document middlewares
+
+// hash password before saving it
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    console.log("error in hashing password", error);
+  }
+});
+
+// compare password if is valid
+// userSchema.methods.isValidPassword = async function (password) {
+//   try {
+//     // Compare provided password with stored hash
+//     return await bcrypt.compare(password, this.password);
+//   } catch (error) {
+//     console.log("Password comparison failed");
+//     return false;
+//   }
+// };
 
 const User = mongoose.model("User", userSchema);
 export default User;
