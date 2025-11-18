057851371e05b5e8834fff25b31a458ace7a7d49
Mohammed Yazji <mw.alyazji@gmail.com>
2025-11-18 11:58:54 +0200

    implement auth routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import authRoutes from "./routes/auth.route.js";
 
 // init my app, and read env //
 dotenv.config();
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 8000;
 // Middleware //
 app.use(cors()); // enable cors [unblock request from client domain]
 app.use(express.json()); // enable read json
+
+// Routes //
+app.use("/api/auth", authRoutes);
 
 // run my application, and connect to DB //
 app.listen(PORT, () => {
